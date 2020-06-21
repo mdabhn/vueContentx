2b52bb373bea359ae6d8b0ab837f1f02cb45ff2a
mdabhn <mdabhn@gmail.com>
2020-06-21 19:58:01 +0600

    v-show v-if

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <Main />
+    <ConditionsList />
   </div>
 </template>
 
 <script>
   import Main from './components/Main'
+  import ConditionsList from './components/ConditionsList'
   export default {
     components: {
       Main,
+      ConditionsList,
     },
   }
 </script>
